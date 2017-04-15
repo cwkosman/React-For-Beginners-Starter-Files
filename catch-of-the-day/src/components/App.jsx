3d51ea6b8a328fff852cb2f50d799cf45e4d8d61
Chuck Kosman <cwkosman@gmail.com>
2017-04-15 11:53:14 -0700

    AddFish form component, submit to state

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -5,14 +5,34 @@ import Inventory from './Inventory.jsx'
 
 
 class App extends React.Component {
-  render () {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    //Initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    //ppdate state
+    const fishes = {...this.state.fishes};
+    //add in new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    //Set state
+    this.setState({ fishes })
+  }
+
+  render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
